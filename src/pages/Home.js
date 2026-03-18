e69976654f6bead65469539f9e585084484b3c64
unknown <andreshf@live.cl>
2026-03-17 22:09:34 -0300

    fix home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import About from "./About";
 import Contact from "./Contact";
 import Gallery from "./Gallery";
 import Team from "./Team";
-import News from "./News";
+//import News from "./News";
 import Subscription from "./Subscription";
 
 function Home() {
